ede096237cc7f21699938735945d770cefd86465
obiukh19 <oleg.obiukh@gmail.com>
2019-03-22 22:54:42 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-openweather/build/precache-manifest.854df3334df5793ba4b8871a806d3081.js"
+  "/react-openweather/precache-manifest.6a248ce099e6bd1fa19a3e7b596764b3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-openweather/build/index.html", {
+workbox.routing.registerNavigationRoute("/react-openweather/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
